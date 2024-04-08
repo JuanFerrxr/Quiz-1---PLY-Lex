7a59fb6efe7d1e7af3f63592da3d98ba0dee1aea
JuanFerrxr <jcferrere1032@gmail.com>
2024-04-08 17:52:07 -0600

    Commit pregunta 3

--- clite.py
+++ clite.py
@@ -6,6 +6,11 @@ tokens = [
 
 t_ignore  = ' \t'
 
+def t_FLOAT(t):
+    r'((((\d+(_\d+)).(\d+(_\d+))))|(((\d+(_\d+)).(\d+(_\d+))))|(((\d+(_\d+)).(\d+(_\d+))))|((\d+(_\d+))))([eE][+-]?(\d+(_\d+)))?'
+    t.value = float(t.value)
+    return t
+
 def t_INT(t):
     r'\d+(_\d+)*'
     t.value = int(t.value)
