87e1d9cb849a927e0720f4bf8f79761392f2d671
JuanFerrxr <jcferrere1032@gmail.com>
2024-04-08 17:09:32 -0600

    Código pregunta 2

--- clite.py
+++ clite.py
@@ -1,5 +1,15 @@
 import ply.lex as lex
 
-tokens = [ 'INT' ]
+tokens = [
+    'INT'
+]
 
 t_ignore  = ' \t'
+
+def t_INT(t):
+    r'\d+(_\d+)*'
+    t.value = int(t.value)
+    return t
+
+def getLexer():
+    return lex.lex()
