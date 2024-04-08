10eebb78fc8966609103f4c9920b91744986c1a9
JuanFerrxr <jcferrere1032@gmail.com>
2024-04-08 17:57:56 -0600

    Corrección pregunta 3

--- clite.py
+++ clite.py
@@ -1,20 +1,22 @@
 import ply.lex as lex
 
-tokens = [
-    'INT'
-]
-
-t_ignore  = ' \t'
+tokens = (
+    'INT',
+    'FLOAT'
+)
 
 def t_FLOAT(t):
-    r'((((\d+(_\d+)).(\d+(_\d+))))|(((\d+(_\d+)).(\d+(_\d+))))|(((\d+(_\d+)).(\d+(_\d+))))|((\d+(_\d+))))([eE][+-]?(\d+(_\d+)))?'
+    r'((((\d+(\_\d+)*)\.(\d+(\_\d+)*)*))|(((\d+(\_\d+)*)*\.(\d+(\_\d+)*)))|(((\d+(\_\d+)*)\.(\d+(\_\d+)*)))|((\d+(\_\d+)*)))([eE][+-]?(\d+(\_\d+)*))?'
     t.value = float(t.value)
     return t
+
 
 def t_INT(t):
     r'\d+(_\d+)*'
     t.value = int(t.value)
     return t
+
+t_ignore  = ' \t'
 
 def getLexer():
     return lex.lex()
